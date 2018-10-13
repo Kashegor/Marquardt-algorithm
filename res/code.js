501cbbdca3bb1b01f7c44f47980a0a72e83603e7
Anton <lalaka32@mail.ru>
2018-10-13 14:43:50 +0300

    56

--- res/code.js
+++ res/code.js
@@ -1,4 +1,5 @@
 //readValues
+
 let exprPolynomHtml = document.getElementById('expr'),
     prettyHtml = document.getElementById('pretty'),
     result = document.getElementById('result'),
@@ -14,20 +15,24 @@ let exprPolynomHtml = document.getElementById('expr'),
     xZero = [document.getElementById('zeroX1'), document.getElementById('zeroX2')],
     accuracy = document.getElementById('accuracy'),
     maxIters = document.getElementById('maxIters'),
+    step5Html = document.getElementById('step5');
+    step6Html = document.getElementById('step6');
     numberIteration = 0,
     numberOfRegulationStrategy = 10000;
 doMainAction();
-//exprPolynomHtml.value = 'sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - sin(pi / 4)^2';
+//exprPolypacknomHtml.value = 'sqrt(75 / 3) + det([[-1, 2], [3, 1]]) - sin(pi / 4)^2';
 //result.innerHTML = math.format(math.eval(exprPolynomHtml.value));
 
 //functions
 
 function doMainAction() {
-    preparation('x^2+2*y^2-6x*y+5x+4');
+    preparation('x^2+2*y^5-6x*y+5x^5+4');
     stepOne([1, 1], 0.1, 1);
     stepTwo();
     stepThree();
     stepFour();
+    stepFive();
+    stepSix();
 }
 
 //---------------------------STEPS------------------------------
@@ -43,10 +48,10 @@ function stepOne(x0, accur, m) {
     xZero[1].value = x0[1];
     accuracy.value = accur;
     maxIters.value = m;
-    let calculatedGradient = getGradient(variables[0], variables[1]);
-    let matrixGessa = getMatrixGessa(variables[0], variables[1]);
-    setPretty(gradient1Html, gradientToString(calculatedGradient));
-    setPretty(gesseHtml, matrixGessa);
+    let calculatedGradient =  new Gradient(variables[0], variables[1]);
+    let matrixGessa = new Matrix(variables[0], variables[1]);
+    setPretty(gradient1Html, calculatedGradient.gradientToString());
+    setPretty(gesseHtml, matrixGessa.getMatrix());
 }
 
 function stepTwo() {
@@ -54,28 +59,38 @@ function stepTwo() {
     outRegulation.innerHTML = '$$μk = μ0 = ' + numberOfRegulationStrategy + '$$';
 }
 function stepThree() {
-    let scope = {
+    let point = {
         x: xZero[0].value,
         y: xZero[1].value
     }
-    let gradientValues = getGradient(variables[0], variables[1]);
-    let gradientInPointX = (math.eval(gradientValues[0], scope)).toString();
-
-	let gradientInPointY = (math.eval(gradientValues[1], scope).toString());
-	let gradientInPoint = Array(gradientInPointX, gradientInPointY);
-	console.log(gradientInPoint);
-    outGradXk.innerHTML =`Градиент функции в ${numberIteration}-ой точке: f(xk) = ` + pointToString(gradientInPoint);
+    let calculatedGradient = new Gradient(variables[0], variables[1]);
+    calculatedGradient.setGradientInPoint(point);
+    outGradXk.innerHTML =`Градиент функции в ${numberIteration}-ой точке: f(xk) = ` + pointToString(calculatedGradient.getGradientInPoint());
 }
 function stepFour(){
-	let scope = {
+	let point = {
         x: xZero[0].value,
         y: xZero[1].value
     }
-    let gradientValues = getGradient(variables[0], variables[1]);
-	let gradXk = math.eval('sqrt(pow(' + gradientValues[0] + ',2)+pow(' + gradientValues[1] + ',2))', scope);
+    let calculatedGradient = new Gradient(variables[0], variables[1]);
+    calculatedGradient.setGradientInPoint(point);
+	let gradXk = calculatedGradient.getLength().toString();
     outGradXkLength.innerHTML =`Градиент функции в ${numberIteration}-ой точке: |f(xk)| =` + gradXk;
     outGradXkLength.innerHTML += '<br> Критерий останова : |▽f(xk)| ≤ ε ';
     outGradXkLength.innerHTML += '<br>'+ gradXk + " " + getSignOfСomparison(gradXk, accuracy.value) + " " + accuracy.value;
+}
+function stepFive() {
+    step5Html.innerHTML = 'Критерий останова :  k ≥ M ';
+    step5Html.innerHTML += '<br>'+ numberIteration + " " + getSignOfСomparison(numberIteration, maxIters.value) + " " + maxIters.value;
+}
+function stepSix(){
+    let point = {
+        x: xZero[0].value,
+        y: xZero[1].value
+    }
+    let matrixGessa = new Matrix(variables[0], variables[1]);
+    matrixGessa.setPoint(point);
+    setPretty(step6Html, matrixGessa.getInPoint());
 }
 
 //--------------------------CALCULATIIONS-------------------------------------
@@ -101,13 +116,6 @@ function unique(arr) {
     return Object.keys(obj); // или собрать ключи перебором для IE8-
 }
 
-function getGradient(first, second) {
-    let peremOne = calculateDerivative(first).toString();
-    let peremTwo = calculateDerivative(second).toString()
-    console.log(Array(peremOne, peremTwo));
-    return Array(peremOne, peremTwo); 
-}
-
 function calulateDoubleDerivative(first, second) {
 
     let scope = {
@@ -127,20 +135,56 @@ function calculateDerivative(perem) {
     return math.eval('derivative(func, perem)', scope);
 }
 
-//-------------------------------VIEW-----------------------------------------
+//------------------------------CLASES(OBJECTS)-------------------------------
 
-function gradientToString(arrayGradient){
-    return '(' + arrayGradient[0] + ')*i+' + '(' + arrayGradient[1] + ')*j';
+function Gradient(_first, _second){
+    this.first = calculateDerivative(_first);
+
+    this.second = calculateDerivative(_second);
+    this.point = [0,0];
+    
+    this.gradientToString = function(){
+        return '(' + this.first.toString() + ')*i+' + '(' + this.second.toString() + ')*j';
+    }
+    this.setGradientInPoint = function(_point){
+        let gradientInPointX = (math.eval(this.first.toString(), _point)).toString();
+        let gradientInPointY = (math.eval(this.second.toString(), _point).toString());
+        this.point = [gradientInPointX, gradientInPointY];
+    }
+    this.getGradientInPoint = function(){
+        return this.point;
+    }
+    this.getLength = function(){
+        return math.eval('sqrt(pow(' + this.point[0] + ',2)+pow(' + this.point[1] + ',2))')
+    }
 }
+function Matrix(first,second) {
+    this.peremOne = calulateDoubleDerivative(first, first);
+    this.peremTwo = calulateDoubleDerivative(first, second);
+    this.peremThree = calulateDoubleDerivative(second, second);
+    this.point = [1,1,1,1]
+
+    this.setPoint = function(_point){
+        let gradientInPointXX = (math.eval(this.peremOne.toString(), _point)).toString();
+        let gradientInPointYY = (math.eval(this.peremThree.toString(), _point).toString());
+        let gradientInPointXY = (math.eval(this.peremTwo.toString(), _point).toString());
+        this.point = [gradientInPointXX,gradientInPointXY, gradientInPointXY, gradientInPointYY];
+    }
+    this.getInPoint = function(){
+        return this.stringMatrix(this.point[0],this.point[1],this.point[2],this.point[3]);
+    }
+    this.getMatrix = function(){
+        
+        return this.stringMatrix(this.peremOne,this.peremTwo,this.peremTwo,this.peremThree);
+    }
+    this.stringMatrix = function(perem1,perem2,perem3,perem4){
+        return '[[' + perem1 + ',' + perem2 + '], [' + perem3 + ', ' + perem4 + ']]';
+    }
+}
+
+//-------------------------------VIEW-----------------------------------------
 function setPretty(element, value) {
     element.innerHTML = '$$' + math.parse(value).toTex({parenthesis: parenthesis}) + '$$';
-}
-
-function getMatrixGessa(first, second) {
-    let peremOne = calulateDoubleDerivative(first, first);
-    let peremTwo = calulateDoubleDerivative(first, second);
-    let peremThree = calulateDoubleDerivative(second, second);
-    return '[[' + peremOne + ',' + peremTwo + '], [' + peremTwo + ', ' + peremThree + ']]';
 }
 function pointToString(coordinates){
 	let stringCoord = "(";
